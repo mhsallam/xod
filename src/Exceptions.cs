5fb5038069c1320eae98fda59925995b1ad09f90
Mohammed Sallam <mhisallam@outlook.com>
2016-10-18 15:07:10 +0300

    Updating user guide, miner Exceptions.cs changes

--- src/Exceptions.cs
+++ src/Exceptions.cs
@@ -16,7 +16,7 @@ namespace Xod
     }
     public class MissingPrimaryKeyValueException : Exception
     {
-        public MissingPrimaryKeyValueException() : base("The object you are trying to persist is lacking a primary key property.") { }
+        public MissingPrimaryKeyValueException() : base("The object you are trying to persist is lacking a primary key value.") { }
     }
     public class PropertyKeyNameException : Exception
     {
@@ -37,10 +37,10 @@ namespace Xod
     {
         public AutonumberDataTypeException() : base("Wrong auto number datatype. This feature can only be applyed on numeric datatype.") { }
     }
-    public class ReservedPrimaryKeyException : Exception
-    {
-        public ReservedPrimaryKeyException() : base("The primary key value is reserved by another object.") { }
-    }
+    //public class ReservedPrimaryKeyException : Exception
+    //{
+    //    public ReservedPrimaryKeyException() : base("The primary key value is reserved by another object.") { }
+    //}
     public class ReservedUniqueKeyException : Exception
     {
         public ReservedUniqueKeyException() : base("One or more unique key value is reserved by another object.") { }
